241c69a57a50cddbc5ca4a10bc624f2739559113
moon <moonlt93@naver.com>
2022-09-30 23:42:55 +0900

    fastlms 커밋 설정

--- src/main/java/com/zerobase/zerolms/ZeroLmsApplication.java
+++ src/main/java/com/zerobase/zerolms/ZeroLmsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ZeroLmsApplication {
-
+    // 2021.09.30
     public static void main(String[] args) {
         SpringApplication.run(ZeroLmsApplication.class, args);
     }
