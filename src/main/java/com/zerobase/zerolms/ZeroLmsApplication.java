a49c5bc9669fd590c53abbb4175d36955951d650
moon <moonlt93@naver.com>
2022-09-30 23:36:06 +0900

    fastlms 초기화 설정

---
+++ src/main/java/com/zerobase/zerolms/ZeroLmsApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.zerolms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroLmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroLmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zerobase/zerolms/ZeroLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.zerolms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroLmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
